e9edc42e15d772591eabce9310a6b7860b35eafe
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-12-02 13:35:20 -0600

    Update GrkAccelTable.hpp

--- framework/STD_CPP17/GrkAccelTable.hpp
+++ framework/STD_CPP17/GrkAccelTable.hpp
@@ -1,1 +1,18 @@
+/*GrkAccelTable.hpp*/
 
+#ifndef _GRK_ACCEL_TABLE_HPP_
+#define _GRK_ACCEL_TABLE_HPP_
+
+#include <string>
+
+class GrkAccelTable
+{
+   public:
+      GrkAccelTable();
+      virtual ~GrkAccelTable();
+
+   const GrkAccelTable& GrkAccelConnectWindow(const GrkWindow& window, bool once);
+
+};
+
+#endif
